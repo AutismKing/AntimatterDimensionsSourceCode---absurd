f695dbd9021700abfc823f6fcc5ecb79a528b62f
Person885 <keyboaqwerty@gmail.com>
2026-02-27 14:46:05 -0700

    Update ClassicGalacticDimensionTab.vue

--- src/components/tabs/Galactic-dimensions/ClassicGalacticDimensionTab.vue
+++ src/components/tabs/Galactic-dimensions/ClassicGalacticDimensionTab.vue
@@ -1,5 +1,5 @@
 <script>
-import GalacticDimensionRow from "./ClassicGalacticDimensionRow";
+import GalacticDimensionRow from "./ClassicGalacticDimensionsRow";
 import PrimaryButton from "@/components/PrimaryButton";
 
 export default {
