70946efb13cfbddb09b1ede466f567772b3ab946
AutismKing <antalyk007@gmail.com>
2026-03-01 18:38:56 +0000

    catch up for real

--- src/core/secret-formula/absurdity/break-eternity.js
+++ src/core/secret-formula/absurdity/break-eternity.js
@@ -2,10 +2,58 @@
 
 
 
-//export const breaketernityupgrade = [
-   // {
-    //  id: 3
-   //   cost: 10000000
-   //   effect: () () => 6
-   // }
-//]
+export const breaketernityupgrade = [
+    {
+      id: 3
+      cost: 100000000
+      description: Singularities are 500% stronger
+      effect: () => 6
+    }
+    {
+      id: 4
+      cost: 10000
+      description: All NC, IC and EC times and time sums are always 1e-300ms.
+      effect:
+    } 
+    {
+      id: 5
+      cost: 10000
+      description: All infinity, eternity and reality best times are always 1e-300ms.
+      effect: 
+   }
+   {
+     id: 9
+     cost: 100000
+     description: All antimatter dimensions gain a power based on your infinity power
+     effect: log 10 inf pow then 20th root 
+   }
+   {
+     id: 10
+     cost: 100000
+     description: All infinity dimensions gain a power based on your time shards
+     effect: log 10 time shards then 20th root
+   }
+   {
+     id: 11
+     cost: 100000
+     description: All time dimensions gain a power based on your dark matter
+     effect: log 10 dark matter then 20th root
+   }
+   {
+     id: 12
+     cost: 100000
+     description: All dark matter dimensions gain a power based on your galactic power
+     effect: log 10 gal pow then 20th root
+   }
+   {
+     id: 13
+     cost: 100000
+     description: All galactic dimensions gain a multiplier based on your antimatter
+     effect: log 10 antimatter then 40th root
+   }
+   {
+     id: 14
+     cost: 10000
+     description: IP is powered based on your replicanti
+     effect: log 10 Replicanti then 20th root
+]
