133c608644382117544bff15d40a0a5f3812248c
Person885 <keyboaqwerty@gmail.com>
2026-03-02 19:38:23 -0700

    Fix countdown calculation to use toNumber()

--- src/components/tabs/normal-achievements/NormalAchievementsTab.vue
+++ src/components/tabs/normal-achievements/NormalAchievementsTab.vue
@@ -73,7 +73,7 @@ export default {
       const gameSpeedupFactor = getGameSpeedupFactor();
       this.achievementPower = Achievements.power;
       this.achTPEffect = RealityUpgrade(8).config.effect();
-      this.achCountdown = Achievements.timeToNextAutoAchieve / gameSpeedupFactor;
+      this.achCountdown = Achievements.timeToNextAutoAchieve / gameSpeedupFactor.toNumber();
       this.totalCountdown = ((Achievements.preReality.countWhere(a => !a.isUnlocked) - 1) * Achievements.period +
         Achievements.timeToNextAutoAchieve) / gameSpeedupFactor.toNumber();
       this.missingAchievements = Achievements.preReality.countWhere(a => !a.isUnlocked);
