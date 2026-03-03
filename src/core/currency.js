e9def409cf6bd1fbfd9e8c901032b132bab35af0
Person885 <keyboaqwerty@gmail.com>
2026-03-02 17:27:52 -0700

    Change Currency.remnants to extend DecimalCurrency

--- src/core/currency.js
+++ src/core/currency.js
@@ -452,7 +452,7 @@ Currency.singularities = new class extends DecimalCurrency {
   set value(value) { player.celestials.laitela.singularities = value; }
 }();
 
-Currency.remnants = new class extends NumberCurrency {
+Currency.remnants = new class extends DecimalCurrency {
   get value() { return player.celestials.pelle.remnants; }
   set value(value) { player.celestials.pelle.remnants = value; }
 }();
