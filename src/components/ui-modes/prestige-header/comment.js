6b0e486a1e0e300cf386b503a8ebe4b2da23cc4e
AutismKing <antalyk007@gmail.com>
2026-05-31 14:41:22 +0100

    also made some changes in time-studies.css

---
+++ src/components/ui-modes/prestige-header/comment.js
@@ -0,0 +1,13 @@
+/* <script>
+export default {
+  name: "AbsurdityButton",
+  data() {
+    return {
+      canAbsurdity: false,
+      showSpecialEffect: false,
+      hasAbsurdityStudy: false,
+      starsGained: new Decimal(),
+      projectedGS: new Decimal(),
+      AbsurdityTime: 0,
+      nextStarRM: 0,
+      nextStariM: 0, */
---
+++ src/core/colossals/quotes.js
@@ -0,0 +1,15 @@
+/* Genox: "Fascinating... I never expected a non-colossal to reach here... to this place." 
+"You have had fun... with your celestials, but now you will meet the true lords of this unedning multiverse, we the colossals.
+I am Genox, the Colossal of Intelligence.
+You may have been able to outsmart Pelle, but I am far cleverer then he. You will never be able to outsmart me.
+...and if that your intelligence need not matter...that you can simply outpower me... then you are sorely mistaken
+For with me is my partner
+
+Omega: Greetings Destroyer, I am Omega, the Colossal of Power.
+Prepare for the end of your existence.
+...
+Genox:...
+Omega:...
+Genox: Oh you were done? I mean I gave a whole speech but-
+Omega: Oh right cause they need a speech that makes sense-
+Genox: Oh don't you start-*/
